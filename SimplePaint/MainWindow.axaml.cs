29ecc9d14e6559b26953034ecf2704c233f33d20
Maximum Code <maximum2000@hotmail.com>
2020-11-08 20:49:32 -0800

    Add project files.

---
+++ SimplePaint/MainWindow.axaml.cs
@@ -0,0 +1,53 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using System.Diagnostics;
+
+namespace SimplePaint
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        protected async void Open_Menu(object sender, RoutedEventArgs eventArgs)
+        {
+            try
+            {
+                // open file dialog
+                OpenFileDialog openFile = new OpenFileDialog();
+
+                openFile.AllowMultiple = false;
+                openFile.Filters.Add(new FileDialogFilter() { Name = "Images", Extensions = { "jpg", "png", "gif" } });
+
+                string[] files = await openFile.ShowAsync((Window)VisualRoot);
+
+                if (files != null && files.Length > 0)
+                {
+                    var imageControl = this.FindControl<Image>("ImageSrc");
+
+                    if (imageControl != null)
+                    {
+                        imageControl.Source = new Bitmap(files[0]);
+                    }
+                }
+            }
+            catch (System.Exception exception)
+            {
+                Debug.WriteLine(exception.Message);
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
